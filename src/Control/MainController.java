51ffe281b6155ebedc59a1c7c6ee780333d43a34
amasso99 <khouri.oussama@gmx.de>
2017-03-02 19:20:18 +0100

    added quick sort algorithm

--- src/Control/MainController.java
+++ src/Control/MainController.java
@@ -255,9 +255,34 @@ public class MainController {
         int j = end;
         int middle =  (i + j) / 2;
         int pivot = moddedArray[middle].getNumber();
-
         //Beginn des Zaubers
             //TODO 05: Programmiere den rekursiven Quicksortalgorithmus. Halte dich an den hier vorgegeben Rahmen.
+
+            while(i <= j){
+                loops++;
+                while(moddedArray[i].getNumber() < pivot){
+                    loops++;
+                    i++;
+                }
+
+                while(moddedArray[j].getNumber() > pivot){
+                    loops++;
+                    j--;
+                }
+
+                if(i <= j){
+                    switchBalls(i,j);
+                    i++;
+                    j--;
+                }
+            }
+
+            if(start < j){
+                quickSortRekursiv(start,j);
+            }
+            if(i < end){
+                quickSortRekursiv(i,end);
+            }
         //Ende des Zaubers
     }
 
